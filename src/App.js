fbb33bb6d23d9db51edad80138d63abceeb640af
teomankirma <83593653+teomankirma@users.noreply.github.com>
2023-03-24 17:43:13 +0300

    feat: listing items from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [astronauts, setAstronauts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://api.open-notify.org/astros.json")
+      .then((res) => res.json())
+      .then((data) => setAstronauts(data.people))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const astronautsList = astronauts.map((astronaut, index) => (
+    <li key={index}>
+      <p>
+        {astronaut.name} Craft: {astronaut.craft}
+      </p>
+    </li>
+  ));
+
   return (
     <div>
-      <h1>hello world</h1>
+      <ul>{astronautsList}</ul>
     </div>
   );
 }
