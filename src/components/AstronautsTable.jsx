20cbcf68cced04bdb41564d8d2acf30685fe2d63
teomankirma <83593653+teomankirma@users.noreply.github.com>
2023-03-27 19:15:34 +0300

    feat: alert boxes for success and error

--- src/components/AstronautsTable.jsx
+++ src/components/AstronautsTable.jsx
@@ -7,20 +7,29 @@ import {
   Button,
   Center,
   Space,
+  Alert,
 } from "@mantine/core";
+import Loading from "react-fullscreen-loading";
 import React, { useState } from "react";
 
 function AstronautsTable() {
   const [astronauts, setAstronauts] = useState([]);
+  const [loading, setLoading] = useState();
+  const [successAlert, setSuccessAlert] = useState(false);
+  const [errorAlert, setErrorAlert] = useState(false);
 
   const handleClick = async function () {
+    setLoading(true);
     try {
       const result = await fetch("http://api.open-notify.org/astros.json");
       const data = await result.json();
 
       setAstronauts(data.people);
+      setLoading(false);
+      setSuccessAlert(true);
     } catch (err) {
-      console.log(err);
+      setLoading(false);
+      setErrorAlert(true);
     }
   };
 
@@ -42,13 +51,14 @@ function AstronautsTable() {
   ));
 
   return (
-    <div>
+    <>
       <Space h="lg" />
       <Center className="api-call">
         <Button color="dark" radius="xl" size="md" onClick={handleClick}>
           API Call
         </Button>
       </Center>
+      <Loading loading={loading} background="#2ecc71" loaderColor="#3498db" />
       <Space h="xl" />
       <Center>
         <ScrollArea>
@@ -63,7 +73,33 @@ function AstronautsTable() {
           </Table>
         </ScrollArea>
       </Center>
-    </div>
+      <Space h="lg" />
+      <Center>
+        {successAlert ? (
+          <Alert
+            title="Successful!"
+            color="green"
+            withCloseButton
+            variant="filled"
+            onClose={() => setSuccessAlert(false)}
+            styles={{ width: "400px" }}
+          >
+            Your API call was successful!
+          </Alert>
+        ) : null}
+        {errorAlert ? (
+          <Alert
+            title="Oops!"
+            color="red"
+            withCloseButton
+            variant="filled"
+            onClose={() => setErrorAlert(false)}
+          >
+            Something went wrong! Please try again!
+          </Alert>
+        ) : null}
+      </Center>
+    </>
   );
 }
 
